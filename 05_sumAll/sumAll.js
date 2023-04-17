e604dca244e152e045b2aa09085281135c582ebe
johnedwardlee <50990111+johnedwardlee@users.noreply.github.com>
2023-04-16 23:13:45 -0400

    Commit my answers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  )
+    return 'ERROR';
+  let sum = 0;
+  let high = num2;
+  let low = num1;
+  if (low > high) {
+    let temp = low;
+    low = high;
+    high = temp;
+  }
+  for (let i = low; i <= high; ++i) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
